ecd94b744d2348d4df5d85a69acfd6ff65c59513
BoyCanad <boycanadian565@gmail.com>
2025-06-22 21:58:22 +0800

    Update index.js for voice move fix 3

--- index.js
+++ index.js
@@ -29,7 +29,20 @@ app.use(bodyParser.json());
 // Example Roblox-to-Discord user mapping (replace with real system or database)
 const robloxToDiscord = {
   "LanceDainiel123": "lancesfp",
-  "RobloxUser2": "UserTwo#4567"
+  "meijikko": "viancaloveskisshia"
+  "lenieyel": "lenie_yel",
+  "jongwon447": "ivangino__94395"
+  "abrielqt": "itzmekennethcanaynay24",
+  "sorbet_moonrabbit": "maxwellisinthehouse_03221"
+  "JhiroMalabanaa": "jjiru_.",
+  "cboyxzz": "cboyxzz._80287"
+  "Rex118898": "rey880050",
+  "cherryblossomon_top": "hana090942"
+  "jggjkgkgkgkg": "jggjkgkgkgkg",
+  "soa_zja": "ashleng0882"
+  "hbt_zoro": "ramen.fxdd",
+  "nhiell_5": "Cadena626273"
+  "shuveeeishereyall": "maxwellisinthehouse_03221"
 };
 
 // Move user to requested voice channel
